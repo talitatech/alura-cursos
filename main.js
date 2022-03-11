eb53bbabcbf00fccae978590b838f2532fc0e60d
Talita Santos <tatha.talita@gmail.com>
2022-03-10 22:01:04 -0300

    feat: calculadora aritmetica via console

---
+++ main.js
@@ -0,0 +1,18 @@
+var nome = "Talita"
+
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 7
+var notaDoTerceiroBimestre = 4
+var notaDoQuartoBimestre = 2 
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem vinda " + nome)
+console.log(notaFixada)
+
+// isso é um comentário 
+
+// Revisão
+// variáveis, strings, console.log, toFixed, operações matemáticas, concatenação
